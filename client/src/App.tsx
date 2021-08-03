21680409fecf3152092100f75d95c1804402da6a
GabrielAceti <gabriel.aceti@hotmail.com>
2021-08-03 17:07:23 -0300

    Creating Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Routes from './routes'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">       
-        <h1>TESTE</h1>
-      </header>
-    </div>
+      <Routes />
+    </div>  
   );
 }
 
---
+++ client/src/routes.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import  { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+//Imports from admin
+import Dashboard from './pages/admin/dashboard'
+
+import Products from './pages/admin/products';
+import ProductsEdit from './pages/admin/products/productsEdit';
+import ProductsRegister from './pages/admin/products/productsRegister';
+
+import Users from './pages/admin/users';
+import UsersEdit from './pages/admin/users/userEdit';
+import UsersRegister from './pages/admin/users/userRegister';
+
+//Imports from client
+import Home from './pages/client/home/index'; 
+import ProductsDetails from './pages/client/products/productsDetails';
+
+function Routes() {    
+    return(
+        <BrowserRouter>
+            <Switch>
+                {/*Routes for client*/}              
+                <Route path="/" exact component={Home} />
+                <Route path="/products/:_id" exact component={ProductsDetails} />
+
+                {/*Routs for admin*/}
+                <Route path="/admin" exact component={Dashboard}/>
+                <Route path="/admin/products" exact component={Products}/>
+                <Route path="/admin/products/register" exact component={ProductsRegister}/>
+                <Route path="/admin/products/register/:_id" exact component={ProductsEdit}/>
+                
+                <Route path="/admin/users" exact component={Users}/>
+                <Route path="/admin/users/register" exact component={UsersRegister}/>
+                <Route path="/admin/users/register/:_id" exact component={UsersEdit}/>
+
+            </Switch>
+        </BrowserRouter>
+    )       
+    
+}
+
+export default Routes;
