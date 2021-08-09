480de1c8e340ef47143f22f5226c157873d02685
GabrielAceti <gabriel.aceti@hotmail.com>
2021-08-09 10:28:50 -0300

    Importing cors to app.ts

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,10 @@
 import Express from 'express';
 import Routes from './Routes';
+import cors from 'cors';
 const app = Express();
 
 app.use(Express.json());
+app.use(cors({ origin: true }));
 app.use('/', Routes);
 
 export default app;
