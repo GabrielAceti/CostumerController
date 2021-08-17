9babf895611e173a2d3a21f91a7c8ca24e00c6aa
GabrielAceti <gabriel.aceti@hotmail.com>
2021-08-17 10:49:48 -0300

    Updating routes: Changing names from 'products' to 'costumer'

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -5,9 +5,9 @@ import PrivateRoute from './services/redirectAuthentication';
 //Imports from admin
 import Dashboard from './pages/admin/dashboard'
 
-import Products from './pages/admin/products';
-import ProductsEdit from './pages/admin/products/productsEdit';
-import ProductsRegister from './pages/admin/products/productsRegister';
+import Costumers from './pages/admin/costumers';
+import CostumersEdit from './pages/admin/costumers/costumersEdit';
+import CostumersRegister from './pages/admin/costumers/costumersRegister';
 
 import Users from './pages/admin/users/index';
 import UsersEdit from './pages/admin/users/userEdit';
@@ -17,21 +17,20 @@ import Login from './pages/admin/login'
 
 //Imports from client
 import Home from './pages/client/home/index'; 
-import ProductsDetails from './pages/client/products/productsDetails';
-
+import CostumersDetails from './pages/client/costumers/costumersDetails';
 function Routes() {    
     return(
         <BrowserRouter>
             <Switch>
                 {/*Routes for client*/}              
                 <PrivateRoute path="/" exact component={Home} />
-                <PrivateRoute path="/products/:_id" exact component={ProductsDetails} />
+                <PrivateRoute path="/costumers/:_id" exact component={CostumersDetails} />
 
                 {/*Routs for admin*/}
                 <PrivateRoute path="/admin" exact component={Dashboard}/>
-                <PrivateRoute path="/admin/products" exact component={Products}/>
-                <PrivateRoute path="/admin/products/register" exact component={ProductsRegister}/>
-                <PrivateRoute path="/admin/products/edit/:_id" exact component={ProductsEdit}/>
+                <PrivateRoute path="/admin/costumers" exact component={Costumers}/>
+                <PrivateRoute path="/admin/costumers/register" exact component={CostumersRegister}/>
+                <PrivateRoute path="/admin/costumers/edit/:_id" exact component={CostumersEdit}/>
                 
                 <PrivateRoute path="/admin/users" exact component={Users}/>
                 <PrivateRoute path="/admin/users/register" exact component={UsersRegister}/>
